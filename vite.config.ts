e5847642ed2bec6d77a8cab1856a6dfc58addea9
mahziarravankar1391 <mahziarravankar1391@gmail.com>
2026-09-12 18:59:06 +0330

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,3 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/mazechat/',
+  plugins: [react()],
+})
